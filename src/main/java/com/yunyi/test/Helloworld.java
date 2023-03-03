3999b1b89d6422dec065026609518865571ea72d
wy <yong.wang@peraglobal.com>
2023-03-03 15:34:09 +0800

    second idea commit

--- src/main/java/com/yunyi/test/Helloworld.java
+++ src/main/java/com/yunyi/test/Helloworld.java
@@ -3,5 +3,6 @@ package com.yunyi.test;
 public class Helloworld {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
