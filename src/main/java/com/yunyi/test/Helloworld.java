b95875e79e73a8580d1be2c49c929e2ab9944e29
wy <yong.wang@peraglobal.com>
2023-03-03 20:48:44 +0800

    master提交了一个 测试代码

--- src/main/java/com/yunyi/test/Helloworld.java
+++ src/main/java/com/yunyi/test/Helloworld.java
@@ -5,5 +5,6 @@ public class Helloworld {
         System.out.println("hello git!");
         System.out.println("hello git2!");
         System.out.println("hello git32!");
+        System.out.println("master test");
     }
 }
